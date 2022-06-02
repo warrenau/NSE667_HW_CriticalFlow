c7e36a41a5d480d5650daf5e9751650a02f959b4
warrenau <warrenau@oregonstate.edu>
2022-06-02 09:46:49 -0700

    added critical point calcs. added table with crit point values. added more discussion to report.

--- NSE667_HW_CriticalFlow.py
+++ NSE667_HW_CriticalFlow.py
@@ -24,7 +24,7 @@ s_0 = steamTable.sL_p(35) # initial specific entropy
 # generate pressure values from 0 to P_0
 # use pressure values and entropy to determine corresponding h_m
 # calc G_m for the pressure
-P = np.linspace(0.01,P_0, 100)
+P = np.linspace(0.01,P_0, 10000)
 x = np.zeros(len(P))
 
 # calc rho_m
@@ -49,15 +49,23 @@ for i in range(len(P)):
 
 # G_m -- HEM
 G_m = rho_m * np.sqrt(2*(h_0 - h_m)*1000)
-
+max_HEM = np.amax(G_m)
+index_HEM = np.argmax(G_m)
+Pmax_HEM = P[index_HEM]
 
 # Fauske
 S_fauske = np.sqrt(rho_f / rho_g)
 G_m_fauske = MassFlux(h_0,h_m,rho_g,rho_f,x,S_fauske)
+max_fauske = np.amax(G_m_fauske)
+index_fauske = np.argmax(G_m_fauske)
+Pmax_fauske = P[index_fauske]
 
 # Moody
 S_moody = (rho_f / rho_g)**(1/3)
 G_m_moody = MassFlux(h_0,h_m,rho_g,rho_f,x,S_moody)
+max_moody = np.amax(G_m_moody)
+index_moody = np.argmax(G_m_moody)
+Pmax_moody = P[index_moody]
 
 
 
@@ -74,3 +82,25 @@ plt.ylabel(r'$\frac{P}{P_0}$')
 plt.figlegend(bbox_to_anchor=(1.0,0.9))
 plt.grid(b=True, which='major', axis='both')
 plt.savefig('1-plots/graph_Gm.pdf',transparent=True)
+
+
+
+# table
+# generate latex table
+out_file = open('2-tabs/crit_tab.tex','w')
+out_file.write(
+                '\\begin{table}[htbp]\n'+
+                '\t \centering\n'+
+                '\t \caption{Critical mass flow and back pressure.}\n'+
+                '\t \\begin{tabular}{cccc}\n'+
+                '\t\t \\toprule\n'+
+                '\t\t  & HEM & Fauske & Moody \\\ \n'+
+                '\t\t \midrule \n'+
+                '\t\t $G_m \left(\si{\kilo\gram\per\meter^2\per\second}\\right)$  & '+str(max_HEM)+' & '+str(max_fauske)+' &  '+str(max_moody)+'\\\ \n'+
+                '\t\t $P_{crit}$ (bar) & '+str(Pmax_HEM)+' & '+str(Pmax_fauske)+' &  '+str(Pmax_moody)+' \\\ \n'+
+                '\t\t \\bottomrule \n'+
+                '\t \end{tabular} \n'+
+                '\t \label{tab:crit} \n'+
+                '\end{table}'
+)
+out_file.close
